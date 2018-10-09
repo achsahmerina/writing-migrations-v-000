f4d3d2a8821631d08ea9fec2dfd38ca941a03db3
Achsah Abraham <achsahmp@gmail.com>
2018-10-09 17:50:08 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,10 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "students", force: :cascade do |t|
-    t.string "name"
+  create_table "students", force: :cascade do |student|
+    student.string "name"
+    student.integer "grade"
+    student.datetime "birthdate"
   end
 
 end
